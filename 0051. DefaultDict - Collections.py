f3e5e90574bfe9cf04c755460bff83f16f4580cc
Isha Bansal <isha.bansal1504@gmail.com>
2021-02-22 13:11:07 +0530

    more problems

--- 0051. DefaultDict - Collections.py
+++ 0051. DefaultDict - Collections.py
@@ -1,8 +1,17 @@
 from collections import defaultdict
-d_a=defaultdict(list)
-d_b=defaultdict(list)
-x=input().split()
-n=int(x[0])
-m=int(x[1])
-for i in range(n):
-  pass
+d = defaultdict(list)
+list1=[]
+
+n, m = map(int,input().split())
+
+for i in range(0,n):
+    d[input()].append(i+1) 
+
+for i in range(0,m):
+    list1=list1+[input()]  
+
+for i in list1: 
+    if i in d:
+        print(" ".join( map(str,d[i]) ))
+    else:
+        print(-1)
